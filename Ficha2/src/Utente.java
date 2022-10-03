826316df5629a63ccd9395f2ec204ddf31c2f00e
rodri <rodri@10.180.2.89>
2022-10-03 14:43:52 +0100

    utente versao3 toString

--- Ficha2/src/Utente.java
+++ Ficha2/src/Utente.java
@@ -1,5 +1,9 @@
 /**
  * 
+ */
+
+/**
+ * 
  * @author Rodrigo Magalhães 43202
  * A classe Utente vai ser a classe de criação de objetos utentes
  */
@@ -16,76 +20,6 @@ public class Utente {
 		this.genero = genero;
 		this.idade = idade;
 		this.altura = altura;
-		this.peso = peso;
-	}
-
-	/**
-	 * @return the nome
-	 */
-	public String getNome() {
-		return nome;
-	}
-
-	/**
-	 * @param nome the nome to set
-	 */
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	/**
-	 * @return the genero
-	 */
-	public String getGenero() {
-		return genero;
-	}
-
-	/**
-	 * @param genero the genero to set
-	 */
-	public void setGenero(String genero) {
-		this.genero = genero;
-	}
-
-	/**
-	 * @return the idade
-	 */
-	public int getIdade() {
-		return idade;
-	}
-
-	/**
-	 * @param idade the idade to set
-	 */
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-
-	/**
-	 * @return the altura
-	 */
-	public float getAltura() {
-		return altura;
-	}
-
-	/**
-	 * @param altura the altura to set
-	 */
-	public void setAltura(float altura) {
-		this.altura = altura;
-	}
-
-	/**
-	 * @return the peso
-	 */
-	public int getPeso() {
-		return peso;
-	}
-
-	/**
-	 * @param peso the peso to set
-	 */
-	public void setPeso(int peso) {
 		this.peso = peso;
 	}
 
